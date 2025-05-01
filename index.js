11e54d0a3a9db9ce5ed62dffa96d75cef1fc3233
Joco570 <166834181+Joco570@users.noreply.github.com>
2025-04-30 19:13:52 -0700

    added count, set limit on deck size

--- index.js
+++ index.js
@@ -13,8 +13,18 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 let playerEl = document.getElementById("player-el");
+let countEl = document.getElementById("count-el");
+let cardCount = 0;
 
 playerEl.textContent = player.name + ": $" + player.chips;
+countEl.textContent = "Count: " + cardCount;
+
+function getCount(card) {
+  let value = card.slice(0, -1);
+  if (["2", "3", "4", "5", "6"].includes(value)) return 1;
+  else if (["10", "J", "Q", "K", "A"].includes(value)) return -1;
+  else return 0;
+}
 
 function createDeck() {
   let suits = ["♠", "♥", "♦", "♣"];
@@ -47,9 +57,10 @@ function shuffleDeck() {
     [fullDeck[i], fullDeck[j]] = [fullDeck[j], fullDeck[i]];
   }
   console.log("Deck shuffled");
+  cardCount = 0;
 }
 
-function getCardValue(card) {
+function getCountValue(card) {
   const value = card.slice(0, -1);
   if (["J", "Q", "K"].includes(value)) return 10;
   if (value === "A") return 11;
@@ -60,7 +71,7 @@ function calculateCardValue(cards) {
   let sum = 0;
   let aceCount = 0;
   for (let card of cards) {
-    const v = getCardValue(card);
+    const v = getCountValue(card);
     sum += v;
     if (card.slice(0, -1) === "A") aceCount++;
   }
@@ -72,6 +83,9 @@ function calculateCardValue(cards) {
 }
 
 function drawCard() {
+  const card = fullDeck.pop();
+  cardCount += getCount(card);
+  countEl.textContent = "Count: " + cardCount;
   return fullDeck.pop();
 }
 
@@ -97,7 +111,27 @@ function startGame() {
   let secondCard = drawCard();
   cards.push(firstCard, secondCard);
   sum = calculateCardValue(cards);
+  player.chips -= 10;
+  playerEl.textContent = player.name + ": $" + player.chips;
   renderGame();
+}
+
+function newHand() {
+  if (fullDeck.length > 2) {
+    isAlive = true;
+    hasBlackJack = false;
+    player.chips -= 10;
+    playerEl.textContent = player.name + ": $" + player.chips;
+    cards = [];
+    let firstCard = drawCard();
+    let secondCard = drawCard();
+    cards.push(firstCard, secondCard);
+    sum = calculateCardValue(cards);
+    renderGame();
+  } else {
+    messageEl.textContent =
+      "Not enough cards left in the deck to start a new hand. Please shuffle deck.";
+  }
 }
 
 function renderGame() {
@@ -112,6 +146,8 @@ function renderGame() {
   } else if (sum === 21) {
     message = "You've got Blackjack!";
     hasBlackJack = true;
+    player.chips += 20;
+    playerEl.textContent = player.name + ": $" + player.chips;
   } else {
     message = "You're out of the game!";
     isAlive = false;
